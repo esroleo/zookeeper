69f8205b3d7c42dfcff20dc7aaca01e7c5fab69e
esroleo <esroleo@gmail.com>
2021-02-17 10:54:15 -0500

    get animals fetch command completed

--- server.js
+++ server.js
@@ -204,3 +204,18 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+/// *** Serve route to the animals.html  *** //
+/// *** /animals - intentional to keey page organized and expectation of data *** //
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+/// *** Serve route to the zookeepers.hml
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
